7797671bf13098b7aac56c3b83de99de8f86bc9b
SungKihun <tjdrlgns3293@gmail.com>
2021-08-01 15:31:55 +0900

    UIImagePicker 델리게이트 메소드 구현

--- Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/ViewController.swift
@@ -16,9 +16,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
         picker.allowsEditing = true // 이미지 편집 기능 On
-        
-        // 델리게이트 지정
-        picker.delegate = self
+        picker.delegate = self // 델리게이트 지정
         
         // 이미지 피커 컨트롤러 실행
         self.present(picker, animated: false)
@@ -42,7 +40,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // 이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false) { () in
+            // 이미지를 이미지 뷰에 표시
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
     }
 }
 
